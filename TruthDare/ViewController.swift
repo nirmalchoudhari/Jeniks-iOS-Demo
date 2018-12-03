062aef36d1ff8045cf3c7aedef848bd78047b444
Nirmal Choudhari <nirmalchoudhari@outlook.com>
2018-12-03 09:38:16 +0530

    Add xcode project

---
+++ TruthDare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TruthDare
+//
+//  Created by Nirmal Choudhari on 27/11/18.
+//  Copyright © 2018 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
